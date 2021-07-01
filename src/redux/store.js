96fa22e66ee70a34dbce42474ad3be56b1f879dd
kiran-kumar011 <kirankirankumar56@gmail.com>
2021-07-01 21:23:26 +0530

    adding redux debugger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,22 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {persistStore, persistReducer} from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {rootReducer} from './reducers';
+
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
 };
+
+const middlewareToApply = [];
+
+if (__DEV__) {
+  const createDebugger = require('redux-flipper').default;
+  middlewareToApply.push(createDebugger());
+}
+
+const middlewares = applyMiddleware(...middlewareToApply);
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-const Store = createStore(persistedReducer);
+const Store = createStore(persistedReducer, middlewares);
 export default Store;
 export const Persistor = persistStore(Store);
