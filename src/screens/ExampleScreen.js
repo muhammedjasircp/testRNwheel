b4de8b1ff608a65e5e4e5d54f763a68c183d43fb
kiran-kumar011 <kirankirankumar56@gmail.com>
2021-07-02 14:11:54 +0530

    async-storage debugger in flipper

--- src/screens/ExampleScreen.js
+++ src/screens/ExampleScreen.js
@@ -1,18 +1,51 @@
 // LIBRARIES
-import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, TextInput} from 'react-native';
 import {connect} from 'react-redux';
+import {storeData, getData} from '../utils/asyncStore';
 
 const ExampleScreen = ({dispatch, counter}) => {
+  const [name, setName] = useState(null);
+  const [updatedName, setUpdatedName] = useState(null);
+
+  useEffect(() => {
+    console.log(name, 'name');
+  });
+
   return (
     <View style={styles.container}>
       <View style={styles.btnWrapper}>
+        <View style={styles.headerText}>
+          <Text>Redux Debbuging test</Text>
+        </View>
         <TouchableOpacity onPress={() => dispatch({type: 'INCREMENT'})} style={styles.btn}>
           <Text style={styles.text}>Increment</Text>
         </TouchableOpacity>
         <Text>{counter}</Text>
         <TouchableOpacity onPress={() => dispatch({type: 'DECREMENT'})} style={styles.btn}>
           <Text style={styles.text}>Decrement</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.btnWrapper}>
+        <View style={styles.headerText}>
+          <Text>AsyncStorage debugging test</Text>
+        </View>
+        <View style={{borderWidth: 1, borderColor: 'black'}}>
+          <TextInput onChangeText={(text) => setName(text)} style={styles.textInput} value={name} />
+        </View>
+        <TouchableOpacity onPress={() => storeData('name', name)} style={styles.btn}>
+          <Text style={styles.text}>SET</Text>
+        </TouchableOpacity>
+        <Text>{updatedName ? updatedName : null}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            getData('name').then((res) => {
+              console.log(res, 'fetched name from async store');
+              setUpdatedName(res);
+            });
+          }}
+          style={styles.btn}>
+          <Text style={styles.text}>GET</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -27,8 +60,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   btnWrapper: {
-    height: '15%',
-    width: '60%',
+    marginVertical: 20,
+    width: '80%',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
@@ -39,6 +72,12 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
   },
+  headerText: {
+    marginVertical: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  textInput: {padding: 0, margin: 0, width: 200, color: 'black'},
 });
 
 const mapStateToProps = (state) => {
