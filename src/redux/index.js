adfbe3c3e95ce2487c09f27ea5704712de849776
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2021-07-05 18:54:32 +0530

    rebasing master

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,17 @@
 import {combineReducers} from 'redux';
-const Reducer = combineReducers({});
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const Reducer = combineReducers({
+  counter: counterReducer,
+});
 export const rootReducer = Reducer;
