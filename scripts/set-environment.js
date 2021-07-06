55eb939474c1ab302e7c0dffa579c1743ccd2b9b
MamtaDeswal <mamta.deswal@bigbinary.com>
2021-07-06 18:48:21 +0530

    yarn conflict

--- scripts/set-environment.js
+++
@@ -1,5 +0,0 @@
-#!/bin/node
-const fs = require('fs');
-const environment = process.argv[2];
-const envFileContent = require(`../envs/${environment}.json`);
-fs.writeFileSync('env.json', JSON.stringify(envFileContent, undefined, 2));
