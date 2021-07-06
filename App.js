02ae3fecc84c5323ef0cf7b02dc1620bdd86e3ed
MamtaDeswal <mamta.deswal@bigbinary.com>
2021-07-06 15:05:00 +0530

    path alias for src added

--- App.js
+++ App.js
@@ -2,15 +2,11 @@ import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import Store, { Persistor } from 'src/store';
+import Store, { Persistor } from '@src/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import AppNavigatior from './src/navigation';
+import AppNavigatior from '@src/navigation';
 
 const App = () => {
-  useEffect(() => {
-    // Splashcreen.hide
-  }, []);
-
   return (
     <SafeAreaProvider>
       <Provider store={Store}>
