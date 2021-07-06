65898efb70f038d437428bda3273bb5305b94e32
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2021-07-06 17:50:39 +0530

    fixing conflicts

--- src/screens/ExampleScreen.js
+++ src/screens/ExampleScreen.js
@@ -1,20 +1,49 @@
 // LIBRARIES
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import { storeData, getData } from '../utils/asyncStore';
 
 const ExampleScreen = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const [name, setName] = useState(null);
+  const [updatedName, setUpdatedName] = useState(null);
+
   return (
     <View style={styles.container}>
       <View style={styles.btnWrapper}>
+        <View style={styles.headerText}>
+          <Text>Redux Debbuging test</Text>
+        </View>
         <TouchableOpacity onPress={() => dispatch({ type: 'INCREMENT' })} style={styles.btn}>
           <Text style={styles.text}>Increment</Text>
         </TouchableOpacity>
         <Text>{counter}</Text>
         <TouchableOpacity onPress={() => dispatch({ type: 'DECREMENT' })} style={styles.btn}>
           <Text style={styles.text}>Decrement</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.btnWrapper}>
+        <View style={styles.headerText}>
+          <Text>AsyncStorage debugging test</Text>
+        </View>
+        <View style={{ borderWidth: 1, borderColor: 'black' }}>
+          <TextInput onChangeText={(text) => setName(text)} style={styles.textInput} value={name} />
+        </View>
+        <TouchableOpacity onPress={() => storeData('name', name)} style={styles.btn}>
+          <Text style={styles.text}>SET</Text>
+        </TouchableOpacity>
+        <Text>{updatedName ? updatedName : null}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            getData('name').then((res) => {
+              console.log(res, 'fetched name from async store');
+              setUpdatedName(res);
+            });
+          }}
+          style={styles.btn}>
+          <Text style={styles.text}>GET</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -29,8 +58,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   btnWrapper: {
-    height: '15%',
-    width: '60%',
+    marginVertical: 20,
+    width: '80%',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
@@ -41,6 +70,12 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
   },
+  headerText: {
+    marginVertical: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  textInput: { padding: 0, margin: 0, width: 200, color: 'black' },
 });
 
 export default ExampleScreen;
---
+++ src/utils/asyncStore.js
@@ -0,0 +1,23 @@
+import RNAsyncStorageFlipper from 'rn-async-storage-flipper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+RNAsyncStorageFlipper(AsyncStorage);
+const storeData = async (key, value) => {
+  console.log(key, value, 'async store setddata');
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    // saving error
+  }
+};
+
+const getData = async (key) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
+
+export { storeData, getData };
