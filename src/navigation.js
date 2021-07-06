b20f5fe201b742f61ae87948bcd49d156606a7e2
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2021-07-06 16:45:07 +0530

    rebasing master

--- src/navigation.js
+++ src/navigation.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+<<<<<<< HEAD
+=======
+// SCREENS IMPORT
+>>>>>>> e9ea698 (configured_flipper)
 import ExampleScreen from '@screens/ExampleScreen';
 
 const Stack = createStackNavigator();
