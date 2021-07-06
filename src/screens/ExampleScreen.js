b880eb2859686bc0953948458288a5d4d3829f87
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2021-07-06 12:50:46 +0530

    using useDispatch hook to dispatch actions

--- src/screens/ExampleScreen.js
+++ src/screens/ExampleScreen.js
@@ -1,9 +1,10 @@
 // LIBRARIES
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import {connect} from 'react-redux';
+import {connect, useDispatch} from 'react-redux';
 
-const ExampleScreen = ({dispatch, counter}) => {
+const ExampleScreen = ({counter}) => {
+  const dispatch = useDispatch();
   return (
     <View style={styles.container}>
       <View style={styles.btnWrapper}>
