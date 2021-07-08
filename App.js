0b202b7f06de56dc4a25b960c5d539cac9b25550
Sourav Kumar <sourav.baranwal@gmail.com>
2021-07-09 01:28:18 +0530

    structured redux folder

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import Store, { Persistor } from '@src/store';
+import Store, { Persistor } from '@redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import AppNavigator from '@src/navigation';
 
