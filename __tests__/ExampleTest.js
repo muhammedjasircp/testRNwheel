6317b2e6b6be11e91145d3fdc47ce3f040537b24
MamtaDeswal <mamta.deswal@bigbinary.com>
2021-07-12 17:16:25 +0530

    yarn.lock

--- __tests__/ExampleTest.js
+++ __tests__/ExampleTest.js
@@ -1,4 +1,6 @@
+import 'react-native';
 import React from 'react';
+import {shallow} from 'enzyme';
 import renderer from 'react-test-renderer';
 import Example from '../src/screens/ExampleScreen';
 
